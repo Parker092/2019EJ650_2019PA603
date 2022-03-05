95356fdaa8a7533004929ea158c61d3ec1d85902
Parker092 <diegopalacios654@gmail.com>
2022-03-05 14:43:45 -0600

    Controlador Pedidos PA

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2019EJ650_2019PA603.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+    }
+}
